af2ecf5e6253cad1534778a883799f928bdbe257
marc monjon <sysmarc13@hotmail.com>
2019-02-08 23:39:13 +0100

    Primer caso test

--- cotxox/src/main/java/org/lasencinas/cotxox/carrera/Carrera.java
+++ cotxox/src/main/java/org/lasencinas/cotxox/carrera/Carrera.java
@@ -1,5 +1,25 @@
 package org.lasencinas.cotxox.carrera;
 
 public class Carrera {
+	private String tarjetaCredito = "";
+	private String destino = null;
+
+	public Carrera() {
+
+	}
+
+	public Carrera(String tarjetaCredito) {
+		this.tarjetaCredito = tarjetaCredito;
+
+	}
+
+	public String getDestino() {
+		return this.destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+
+	}
 
 }
--- cotxox/src/test/java/org/lasencinas/cotxox/test/CarreraTest.java
+++ cotxox/src/test/java/org/lasencinas/cotxox/test/CarreraTest.java
@@ -1,14 +1,28 @@
 package org.lasencinas.cotxox.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.cotxox.carrera.Carrera;
+
+
+
 
 public class CarreraTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private Carrera carrera = null;
+	
+	@Before
+	public void init() {
+		 carrera = new Carrera();
 	}
+	@Test
+	public void ComprobarDestino() {
+		carrera = new Carrera();
+		String destino = "Magaluf";
+		carrera.setDestino(destino);
+		assertEquals("Magaluf", carrera.getDestino());
+	}
+	
 
 }
